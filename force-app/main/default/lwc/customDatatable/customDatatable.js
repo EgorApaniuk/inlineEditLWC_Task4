0d19b0966638fe021f6f8234291bd64dc4b31ecc
Egor <egorzeynos@gmail.com>
2022-04-12 14:13:06 +0300

    custom cell template created, custom datatable created

---
+++ force-app/main/default/lwc/customDatatable/customDatatable.js
@@ -0,0 +1,12 @@
+import LightningDatatable from 'lightning/datatable';
+import customCellTemplate from './customCellTemplate.html';
+ 
+export default class CustomDatatable extends LightningDatatable {
+    
+    static customTypes = {
+        customCell: {
+            template: customCellTemplate,
+            typeAttributes: ['recordId', 'rating']
+        }
+    };
+}
--- force-app/main/default/lwc/tableInlineEditLWC/tableInlineEditLWC.js
+++ force-app/main/default/lwc/tableInlineEditLWC/tableInlineEditLWC.js
@@ -1,22 +1,23 @@
 import { LightningElement, track, wire } from 'lwc';
-import getAccounts from '@salesforce/apex/AccountController.getAccounts';
-import deleteAccount from '@salesforce/apex/AccountController.deleteAccount';
 import { refreshApex} from '@salesforce/apex';
 import {getFieldValue, updateRecord} from 'lightning/uiRecordApi';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
+import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+import deleteAccount from '@salesforce/apex/AccountController.deleteAccount';
 import ID_FIELD from '@salesforce/schema/Account.Id';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import RATING_FIELD from '@salesforce/schema/Account.Rating';
 
 const COLUMNS = [
     {label: 'Name', fieldName: 'Name', type: 'text', editable: true},
-    {label: 'Rating', fieldName: 'Rating', type: 'text', editable: true},
-    {label: 'Delete', fieldName: 'Delete'}
+    {label: 'Rating', fieldName: 'Rating', type: 'customCell'},     // custom data type "customCell"    
+    {label: 'Delete', fieldName: 'Delete', fixedWidth: 90, type: 'button-icon', 
+    typeAttributes: {iconName: 'utility:delete', title: 'Delete', onclick:'handleDelete'}}
 ];
 
 export default class TableInlineEditLWC extends LightningElement {
-    @track dat; 
+    @track data; //что будет если убрать @track???
     columns = COLUMNS;
     wiredAccounts;
 
@@ -31,5 +32,9 @@ export default class TableInlineEditLWC extends LightningElement {
         else if (error) {  //ensert tost event here
             console.log(error);
         }
+    }
+    
+    handleDelete(){
+        console.log("delete pushed");
     }
 }
